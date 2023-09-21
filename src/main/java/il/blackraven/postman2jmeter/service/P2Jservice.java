895cd08720a7f79d610eecfcd0d00e7f808d4784
Dmitry Voskresensky <dmitry.m.voskresensky@gmail.com>
2023-09-21 12:18:22 +0300

    Added logging, fix Containerfile

--- src/main/java/il/blackraven/postman2jmeter/service/P2Jservice.java
+++ src/main/java/il/blackraven/postman2jmeter/service/P2Jservice.java
@@ -3,6 +3,8 @@ package il.blackraven.postman2jmeter.service;
 import il.blackraven.postman2jmeter.dto.P2JRequest;
 import il.blackraven.postman2jmeter.dto.P2JResponse;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,6 +21,7 @@ import java.util.function.Consumer;
 
 @Service
 public class P2Jservice implements IP2J {
+    Logger log = LoggerFactory.getLogger(P2Jservice.class);
     @Override
     public P2JResponse convert(P2JRequest request) {
         String postmanConfigBase64 = request.getPostmanConfig();
@@ -39,29 +42,36 @@ public class P2Jservice implements IP2J {
         boolean isWindows = System.getProperty("os.name")
             .toLowerCase().startsWith("windows");
 
+        log.info("Run converter:");
         ProcessBuilder builder = new ProcessBuilder();
         if (isWindows) {
             builder.command("cmd.exe", "/c", "convert-postman-jmeter", "-p", path + "postman.cfg", "-j", path + "jmeterOut.jmx");
         } else {
             builder.command("sh", "-c", "convert-postman-jmeter", "-p", path + "postman.cfg", "-j", path + "jmeterOut.jmx");
         }
+        log.info(builder.command().toString());
         Process process = builder.start();
 
         int exitCode = process.waitFor();
         if (exitCode == 1) {
+            log.info("Convert successful!");
             Path file = Path.of(path, "jmeterOut.jmx");
-            return Files.readAllLines(file).stream().reduce((acc, s) -> acc + "\n" + s).orElse("");
+            String result = Files.readString(file);
+            log.info(result);
+            return result;
         }
         return "";
     }
 
     private String saveConfigToFile(String postmanConfigBase64) throws IOException {
             byte[] postmanConfig = Base64.decodeBase64(postmanConfigBase64);
+            log.info("Base64 postman config: " + postmanConfigBase64);
             String dirname = "out_" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss"));
             String fileName = "postman.cfg";
             Path path = Path.of("/usr", "app", "p2j", "files", dirname);
             Files.createDirectories(path);
             Files.write(Path.of(path.toString(), fileName), postmanConfig);
+            log.info("Saved to file " + path);
             return path.toString();
     }
 }
